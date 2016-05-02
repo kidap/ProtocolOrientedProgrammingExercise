b2ee130daf2d870502554db15a53265c1f78da68
Karlo Pagtakhan <kidap@yahoo.com>
2016-05-02 15:33:00 -0400

    Extending protocols

--- ProtocolOrientedProgramming.playground/Contents.swift
+++ ProtocolOrientedProgramming.playground/Contents.swift
@@ -2,9 +2,15 @@
 
 import UIKit
 
-protocol Bird{
+protocol Bird:BooleanType{
   var name:String {get}
   var canFly: Bool {get}
+}
+
+extension BooleanType where Self:Bird{
+  var boolValue: Bool{
+    return self.canFly
+  }
 }
 
 protocol Flyable{
@@ -46,6 +52,69 @@ extension Bird where Self:Flyable{
 
 var sb = SwiftBird(name: "Swift Bird", version: 1.0)
 sb.canFly
+
+
+enum UnladenSwallow: Bird, Flyable{
+  case African
+  case European
+  case Unknown
+  
+  var name:String{
+    switch self{
+    case .African:
+      return "African"
+    case .European:
+      return "European"
+    case .Unknown:
+      return "What do you mean? African or European"
+    }
+  }
+  
+  var airSpeedVelocity: Double{
+    switch self{
+    case .African:
+      return 10.0
+    case .European:
+      return 9.9
+    case .Unknown:
+      fatalError("You are thrown from the bridge of death!")
+    }
+  }
+  
+}
+
+if UnladenSwallow.African {
+  print("I can fly!")
+} else {
+  print("Guess I’ll just sit here :[")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
